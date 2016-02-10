c1e0ec9a2536a5df0c2f06786fd0c5b82d8073cd
mygethub-99 <tomakeitcount@gmail.com>
2016-02-10 10:49:53 -0600

    Trying to add my files to git.

---
+++ dropallfeb32015.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+#changed the name of database builder and added user
+from feb32015db import Base, Restaurant, MenuItem, User
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('sqlalchemy.engine.base').setLevel(logging.DEBUG)
+echo = True
+
+engine = create_engine('sqlite:///restaurantmapped.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+Base.metadata.drop_all(engine)
+session.commit()

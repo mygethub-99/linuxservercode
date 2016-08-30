561411c18385f0c845497f85048b65358e813fc4
mygethub-99 <tomakeitcount@gmail.com>
2016-08-30 13:44:57 -0500

    Make ready for psotgresql database from sqlite

--- dbpopfeb3.py
+++ dbpopfeb3.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from feb32015db import Restaurant, Base, MenuItem, User
 
-engine = create_engine('sqlite:///restaurantmapped.db')
+#engine = create_engine('sqlite:///restaurantmapped.db')
+engine = create_engine('postgresql://catalog:mydata@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
